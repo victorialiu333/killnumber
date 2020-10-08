ada276fdd32499a18c7431d21f3d86d0bcef30fe
victorialiu333 <34962335728@qq.com>
2020-10-08 10:23:30 +0800

    杀人游戏

---
+++ cmake-build-debug/CMakeFiles/NO_24.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NO_24.dir/main.c.obj"
+  "NO_24.exe"
+  "NO_24.exe.manifest"
+  "NO_24.pdb"
+  "libNO_24.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/NO_24.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/NO_24.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for NO_24.
+# This may be replaced when dependencies are built.
